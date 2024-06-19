376d5be18c267efd7c9a14cd8589c188ca60d2eb
takmoly <takahiro.mori@thinkiesta.com>
2024-06-19 21:49:00 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 体重と身長の変数を定義
+let weight = 68;
+let height = 1.7;
+
+// BMI（体格指数）の計算
+let bmi = weight / (height * height);
+
+// 結果をコンソールに出力
+console.log("体重: " + weight + "kg");
+console.log("身長: " + height + "m");
+console.log("BMI: " + bmi);
